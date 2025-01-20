9bba432ecd613dafadfbd5461cfe06ece66674dc
th4nh-phat09 <thanphat354@gmail.com>
2025-01-20 20:39:37 +0700

    Config rule eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,51 @@
+// module.exports = {
+//   env: { /* Môi trường: browser, Node.js */ },
+//   extends: [ /* Kế thừa các cấu hình sẵn có */ ],
+//   parserOptions: { /* Cài đặt phân tích cú pháp */ },
+//   settings: { /* Cấu hình bổ sung cho plugin */ },
+//   plugins: [ /* Danh sách plugin bổ sung */ ],
+//   rules: { /* Quy tắc kiểm tra tùy chỉnh */ }
+// }
+
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2],
+    'semi': [1, 'never'],
+    'quotes': ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
 }
