85e80ac15685db50dbbf4a4257f6c0fd0f279d69
th4nh-phat09 <thanphat354@gmail.com>
2025-02-22 14:14:47 +0700

    call api get board details

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,13 +2,20 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import Container from '@mui/material/Container'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 const Board = () => {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    const boardId = '67b8341ce359dcdbaf1786e3'
+    fetchBoardDetailsAPI(boardId).then(board => setBoard(board))
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height :'100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
