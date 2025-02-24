e4367cbf90d44d4ff0e8b6539184d51c30e0bb03
th4nh-phat09 <thanphat354@gmail.com>
2025-02-24 16:24:03 +0700

    add toastify

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <CssVarsProvider theme={theme}>
     <CssBaseline />
     <App />
+    <ToastContainer theme="colored" position="bottom-left"/>
   </CssVarsProvider>
   //{/* </React.StrictMode> */}
 )
