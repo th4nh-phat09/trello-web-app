fb0ac6492e1c4f7a5d235e7d14a5cfd0561f5886
th4nh-phat09 <thanphat354@gmail.com>
2025-03-07 15:00:57 +0700

    deploy prod

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,8 @@
 //sort ,const,...
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev')
+  apiRoot = 'http://localhost:8017'
+if (process.env.BUILD_MODE === 'production')
+  apiRoot = 'https://trello-web-api-t01w.onrender.com'
+export const API_ROOT = apiRoot
+// export const API_ROOT = 'http://localhost:8017'
