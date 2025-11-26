a7df73af1cf71f08fcc3161f7885981d856249cf
th4nh-phat09 <thanphat354@gmail.com>
2025-11-27 01:38:54 +0700

    fix env

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,5 @@
 //sort ,const,...
-let apiRoot = ''
-if (process.env.BUILD_MODE === 'dev')
-  apiRoot = 'http://localhost:8017'
-if (process.env.BUILD_MODE === 'production')
-  apiRoot = 'https://trello-web-api-t01w.onrender.com'
-export const API_ROOT = apiRoot
-// export const API_ROOT = 'http://localhost:8017'
+// Sử dụng biến môi trường từ .env file
+// Trong Vite, phải dùng import.meta.env và prefix VITE_
+export const API_ROOT =
+  import.meta.env.VITE_API_ROOT || "http://localhost:8017";
